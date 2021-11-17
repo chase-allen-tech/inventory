64c78b9e32b89ee94e61d88855024e5af14770e5
admin <wdev555@gmail.com>
2021-11-18 00:06:39 +0800

    a

--- contracts/invoice_views.py
+++ contracts/invoice_views.py
@@ -38,7 +38,7 @@ def getDate(date):
 class TraderSalesInvoiceViewOnly(AdminLoginRequiredMixin, View):
     def post(self, *args, **kwargs):
         top_padding_height = int(256 * 0.4)
-        cell_width_list = [ 256 *  10, 256 * 10, int(256 * 11.5), int(256 * 2.0), 
+        cell_width_list = [ 256 *  10, 256 * 12, int(256 * 9.5), int(256 * 2.0), 
                             int(256 * 4.5), int(256 * 7.5), 256 * 10, int(256 * 6.5),
                             int(256 * 13.5), int(256 * 8.5), int(256 * 7.5)]
         cell_height = int(20 * 15)
@@ -248,7 +248,7 @@ class TraderSalesInvoiceViewOnly(AdminLoginRequiredMixin, View):
         ws.row(6).height = space_height
         ws.row(7).height = height_16_5
         # ws.write(6, 0, 'TEL:', primary_text_style)
-        ws.write_merge(7, 7, 0, 1, 'TEL {}'.format(tel), font_11_left)
+        ws.write_merge(7, 7, 0, 1, 'TEL: {}'.format(tel), font_11_left)
         ws.write_merge(7, 7, 2, 5, 'FAX: {}'.format(fax), font_11_left)
 
         ws.row(8).height = height_16_5
